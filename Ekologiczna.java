2dee50f1b5f5441fa540822eeb12bfae03fd9e66
FranciszekSamiec <49732831+FranciszekSamiec@users.noreply.github.com>
2019-05-04 21:59:12 +0200

    Add files via upload

---
+++ Ekologiczna.java
@@ -0,0 +1,28 @@
+package optymalnaBudowa;
+
+/**
+ * Realizuje strategiê ekonomiczn¹ implementuj¹c odpowiednio metodê
+ * kopiujOptymalnyWynik(Wynik wynik, Wynik kandydat).
+ * 
+ * @author fs395216
+ *
+ */
+public class Ekologiczna extends Optymalna {
+
+    public Ekologiczna(Wejscie wejscie) {
+        super(wejscie);
+    }
+
+    /**
+     * Kopiuje lepszy pod wzglêdem d³ugoœci odpadów wynik
+     */
+    @Override
+    public void kopiujOptymalnyWynik(Wynik wynik, Wynik kandydat) {
+
+        if (wynik.dajDlugoscOdpadow().compareTo(kandydat.dajDlugoscOdpadow()) == -1) {
+            kandydat.kopiuj(wynik);
+        }
+
+    }
+
+}
